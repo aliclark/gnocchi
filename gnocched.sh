5803e55a5fa1c4e5fbaf2c3e0607c2c95239e3a7
user <nobody@example.com>
2016-04-04 21:31:55 +0100

    show the command to use

--- gnocched.sh
+++ gnocched.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# something like this
-
-rule="-p tcp -s $1 --dport 22 -j ACCEPT"
+rule="INPUT -p tcp -s $1 --dport 22 --syn -m state --state NEW -m limit --limit 1/hour --limit-burst 1 -j ACCEPT"
 iptables -I $rule
-sleep 5
+sleep 10
 iptables -D $rule
