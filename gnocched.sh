abebaf9430e2d7d7a254a731eaa5538e9a7b5c27
user <nobody@example.com>
2016-04-04 21:53:09 +0100

    clarifying comments

--- gnocched.sh
+++ gnocched.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Make this file owned by root but readable to a "gnocchi" group, and setuid
+# executable like so:
+#
+# -rwsr-s--- 1 root gnocchi /usr/local/bin/gnocched.sh
+
 rule="INPUT -p tcp -s $1 --dport 22 --syn -m state --state NEW -m limit --limit 1/hour --limit-burst 1 -j ACCEPT"
 iptables -I $rule
 sleep 10
+# Could require bash and put this in a trap at exit
 iptables -D $rule
